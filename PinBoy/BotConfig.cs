4dcac64cd9f02554ac68708481ba93b808ed52fa
Justas Dabrila <justasdabrila@gmail.com>
2017-12-25 16:09:54 +0000

    Added CrashWhiteFlagger

--- PinBoy/BotConfig.cs
+++ PinBoy/BotConfig.cs
@@ -2,15 +2,17 @@
 {
     public sealed class BotConfig
     {
-        public BotConfig(string token, ulong pinChannelId, ulong guildId)
+        public BotConfig(string token, ulong pinChannelId, ulong guildId, ulong crashUserId)
         {
             Token = token;
             PinChannelId = pinChannelId;
             GuildId = guildId;
+            CrashUserId = crashUserId;
         }
 
         public string Token { get; }
         public ulong PinChannelId { get; }
         public ulong GuildId { get; }
+        public ulong CrashUserId { get; }
     }
 }
---
+++ PinBoy/CrashWhiteFlagger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace PinBoy
+{
+    /// <summary>
+    /// Purpose: react with the white flag emote every time we find a message from "@Crash" 
+    /// </summary>
+    public sealed class CrashWhiteFlagger
+    {
+        private readonly BotConfig _cfg;
+
+        private readonly IEmote _whiteflagEmote = new Emoji("🏳");
+
+        public CrashWhiteFlagger(IServiceProvider services)
+        {
+            var client = services.ThrowOrGet<DiscordSocketClient>();
+            _cfg = services.ThrowOrGet<BotConfig>();
+
+            client.MessageReceived += ClientOnMessageReceived;   
+        }
+
+        private async Task ClientOnMessageReceived(SocketMessage msg)
+        {
+            if (msg.Author.Id != _cfg.CrashUserId)
+                return;
+
+            if (msg is IUserMessage userMsg)
+            {
+                await userMsg.AddReactionAsync(_whiteflagEmote);
+            }
+        }
+    }
+}
